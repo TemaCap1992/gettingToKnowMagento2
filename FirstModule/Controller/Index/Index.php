78455c5071b7ac95280633b4a58a3b18e9cb33a7
Artem <TemaCap1992@github.com>
2021-08-09 15:39:00 +0300

    Create page motivation

---
+++ FirstModule/Controller/Index/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Amasty\FirstModule\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+
+class Index extends Action
+{
+    public function execute()
+    {
+       die('Greetings Amasty, having spent a week installing Magento2 - I promise not to give up and go through the internship to the end.');
+    }
+}
